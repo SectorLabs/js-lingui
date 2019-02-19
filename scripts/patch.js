409ab559e17f0a678379ff31119744b5fcd91831
Swen Kooij <swen@sectorlabs.ro>
2019-02-19 17:48:25 +0200

    UPD

--- scripts/patch.js
+++ scripts/patch.js
@@ -7,23 +7,26 @@ if (!version || !filename) {
     process.exit(1);
 }
 
-console.log('version', version);
-console.log('filename', filename);
+const rootPackage = JSON.parse(fs.readFileSync(__dirname + '/../package.json'));
+const rootVersion = rootPackage.version;
+const patchedVersion = `${rootVersion}-sl.${version}`;
 
-const patchVersion = input => `${input}-sl.${version}`;
 const patchDependencies = (dependencies) => {
     Object.keys(dependencies).forEach(depName => {
         if (!depName.includes('@sector-labs') && !depName.includes('@lingui')) {
             return;
         }
 
-        dependencies[depName] = patchVersion(dependencies[depName]);
+        dependencies[depName] = patchedVersion;
     });
 };
 
 const package = JSON.parse(fs.readFileSync(filename, 'utf-8'));
 patchDependencies(package.dependencies || {});
 patchDependencies(package.devDependencies || {});
-package.version = patchVersion(package.version);
+package.version = patchedVersion;
+
+console.log('version: ', rootVersion, ' -> ', package.version);
+console.log('filename', filename);
 
 fs.writeFileSync(filename, JSON.stringify(package, null, 4));
