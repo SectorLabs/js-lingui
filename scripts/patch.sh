15920702c183a3fe7c09e11a41f478f786b97003
Swen Kooij <swen@sectorlabs.ro>
2019-02-19 17:35:44 +0200

    Add back scripts to publish/release @sector-labs fork

---
+++ scripts/patch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+this_dir=$(dirname $0)
+
+find build/ -name '*.*' | while read filename; do
+    if [[ $filename = *"package.json"* ]]; then
+        # patch package names in package.json
+        sed -i '.bak' 's/@lingui\//@sector-labs\/lingui-/' "$filename"
+        rm "$filename.bak"
+
+        # patch versions in package.json
+        node "$this_dir/patch.js" "$1" "$filename"
+
+    elif [[ $filename = *".js"* ]]; then
+        # patch imports in js files
+        sed -i '.bak' 's/require("@lingui\//require("@sector-labs\/lingui-/' "$filename"
+        rm "$filename.bak"
+    fi
+done
